915804977d92aff92ee7bb5ffe56e5d591df723f
Joey Bruno <brunojoey12@gmail.com>
2020-05-02 17:04:58 -0500

    Created build folder contents from user-directory folder.

---
+++ precache-manifest.feef54b6c0460cf6a5ab42c58524dfb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4e65641b7ecb24ce772653662f5790d",
+    "url": "/brunojoey/user-directoryindex.html"
+  },
+  {
+    "revision": "a64ed358877a30ab9647",
+    "url": "/brunojoey/user-directorystatic/css/main.15b8233c.chunk.css"
+  },
+  {
+    "revision": "644b93fbff12784868b5",
+    "url": "/brunojoey/user-directorystatic/js/2.f5619840.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/brunojoey/user-directorystatic/js/2.f5619840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a64ed358877a30ab9647",
+    "url": "/brunojoey/user-directorystatic/js/main.326d90b2.chunk.js"
+  },
+  {
+    "revision": "0900a0460cf425f47ded",
+    "url": "/brunojoey/user-directorystatic/js/runtime-main.7d07e88e.js"
+  }
+]);
